dbe491dc2e1a164aa0fb5b4208deff64d29596d5
Iliailineya <ilavituk011@gmail.com>
2023-06-27 08:30:07 +0300

    EightHomework

--- src/main/java/org/example/eightHomework/Main.java
+++ src/main/java/org/example/eightHomework/Main.java
@@ -1,63 +1,13 @@
 package org.example.eightHomework;
 
-import java.io.*;
-import java.nio.file.Files;
-import java.util.List;
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         String rootDirectory = "src/main/java/org/example/eightHomework/resources/";
+        TraverseDirectory.createDirectory(rootDirectory, 8);
         loop(rootDirectory);
-    }
-
-    public static void traverseDirectory(File directory, boolean writeFile) {
-        if (!directory.isDirectory()) {
-            System.out.println(directory.getAbsolutePath() + " is not a directory.");
-            return;
-        }
-
-        File[] files = directory.listFiles();
-        if (files != null) {
-            for (File file : files) {
-                if (file.isDirectory()) {
-                    traverseDirectory(file, writeFile);
-                } else {
-                    System.out.println(file.getName());
-                    if (writeFile) {
-                        writeToFile(file);
-                    } else {
-                        readFromFile(file);
-                    }
-                }
-            }
-        }
-    }
-
-    public static void writeToFile(File file) {
-        try (BufferedWriter writer = Files.newBufferedWriter(file.toPath())) {
-            System.out.println("Enter text to write to file (press Enter to finish):");
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            String line;
-            while ((line = reader.readLine()) != null && !line.isEmpty()) {
-                writer.write(line);
-                writer.newLine();
-            }
-            System.out.println("Text has been written to file: " + file.getAbsolutePath());
-        } catch (IOException e) {
-            System.out.println("Error writing to file: " + file.getAbsolutePath());
-        }
-    }
-
-    public static void readFromFile(File file) {
-        try {
-            List<String> lines = Files.readAllLines(file.toPath());
-            for (String line : lines) {
-                System.out.println(line);
-            }
-        } catch (IOException e) {
-            System.out.println("Error reading file: " + file.getAbsolutePath());
-        }
     }
 
     public static void continueLoop(String rootDirectory) {
@@ -84,11 +34,11 @@ public class Main {
 
             switch (choice) {
                 case 1:
-                    traverseDirectory(new File(rootDirectory), true);
+                    TraverseDirectory.traverseDirectory(new File(rootDirectory), true);
                     continueLoop(rootDirectory);
                     return;
                 case 2:
-                    traverseDirectory(new File(rootDirectory), false);
+                    TraverseDirectory.traverseDirectory(new File(rootDirectory), false);
                     continueLoop(rootDirectory);
                     return;
                 default:
